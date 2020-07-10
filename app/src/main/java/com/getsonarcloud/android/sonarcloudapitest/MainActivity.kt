5cb4dfc7b4426f38f297ab3934e78e0b4c15eaf0
Michael A. Leonetti <mleonetti@getsonarcloud.com>
2020-07-10 15:48:59 -0400

    Initial commit.

---
+++ app/src/main/java/com/getsonarcloud/android/sonarcloudapitest/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.getsonarcloud.android.sonarcloudapitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.getsonarcloud.android.sdk.Sonarcloud
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button)!!.setOnClickListener({
+            popUpSonarcloud()
+        })
+    }
+
+    fun popUpSonarcloud() {
+        Sonarcloud.instance.presentWindow()
+    }
+}
